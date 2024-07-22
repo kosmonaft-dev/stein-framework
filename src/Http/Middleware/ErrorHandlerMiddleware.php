0f4a9c497d9c4437d57005646bfa776f6e759aba
debuss-a <alexandre@kosmonaft.dev>
2024-07-22 23:02:36 +0200

    feat(log): added logger in ErrorHandlerMiddleware.php

--- src/Http/Middleware/ErrorHandlerMiddleware.php
+++ src/Http/Middleware/ErrorHandlerMiddleware.php
@@ -2,7 +2,9 @@
 
 namespace Stein\Framework\Http\Middleware;
 
+use ErrorException;
 use Laminas\Diactoros\Response\JsonResponse;
+use Psr\Log\LoggerInterface;
 use Psr\Http\Message\{ResponseInterface, ServerRequestInterface};
 use Psr\Http\Server\{MiddlewareInterface, RequestHandlerInterface};
 use Stein\Framework\Http\Error\ApiCallException;
@@ -12,14 +14,26 @@ use Throwable;
 class ErrorHandlerMiddleware implements MiddlewareInterface
 {
 
+    public function __construct(
+        protected LoggerInterface $logger
+    ) {}
+
     /**
      * @inheritDoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         try {
-            return $handler->handle($request);
+            $this->setErrorHandler();
+
+            $response = $handler->handle($request);
         } catch (Throwable $e) {
+            $this->logger->error($e->getMessage(), [
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'code' => $e->getCode()
+            ]);
+
             $problem_details = new ProblemDetails();
             $problem_details->detail = $e->getMessage();
             $problem_details->instance = $request->getUri()->getPath();
@@ -37,7 +51,23 @@ class ErrorHandlerMiddleware implements MiddlewareInterface
                 $problem_details->title = 'Internal Server Error';
             }
 
-            return new JsonResponse($problem_details);
+            $response = new JsonResponse($problem_details);
         }
+
+        restore_error_handler();
+
+        return $response;
+    }
+
+    protected function setErrorHandler(): void
+    {
+        set_error_handler(function(int $errno, string $errstr, string $errfile, int $errline): void {
+            if (!(error_reporting() & $errno)) {
+                // error_reporting does not include this error
+                return;
+            }
+
+            throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+        });
     }
 }
